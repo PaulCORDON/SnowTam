352733104005720dec46c9e2869a30e9d579ff72
Paul <paul.cordon@laposte.net>
2018-11-13 11:40:57 +0100

    navigation entre les pages

--- SnowTam/ProjetSnowTam/app/src/main/java/com/ensim/projetsnowtam/MainActivity.java
+++ SnowTam/ProjetSnowTam/app/src/main/java/com/ensim/projetsnowtam/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ensim.projetsnowtam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button search = (Button)findViewById(R.id.searchBtn);
 
+
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
