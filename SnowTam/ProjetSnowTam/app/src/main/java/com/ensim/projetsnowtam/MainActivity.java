a577e188de64c3ddfe231e16aa82409670d7e6e0
Kilian sanchez <kilian0925@gmail.com>
2018-11-15 18:00:06 +0100

    Ajouter/Supprimer les aeroports

--- SnowTam/ProjetSnowTam/app/src/main/java/com/ensim/projetsnowtam/MainActivity.java
+++ SnowTam/ProjetSnowTam/app/src/main/java/com/ensim/projetsnowtam/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -19,12 +21,33 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
+    public int nbrAirport = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button search = (Button)findViewById(R.id.searchBtn);
 
+
+        final EditText airport1 = (EditText) findViewById(R.id.airport1);
+        final EditText airport2 = (EditText) findViewById(R.id.airport2);
+        final EditText airport3 = (EditText) findViewById(R.id.airport3);
+        final EditText airport4 = (EditText) findViewById(R.id.airport4);
+        final EditText airport5 = (EditText) findViewById(R.id.airport5);
+        final EditText airport6 = (EditText) findViewById(R.id.airport6);
+
+
+        final ImageView close2 = (ImageView) findViewById(R.id.close2);
+        final ImageView close3 = (ImageView) findViewById(R.id.close3);
+        final ImageView close4 = (ImageView) findViewById(R.id.close4);
+        final ImageView close5 = (ImageView) findViewById(R.id.close5);
+        final ImageView close6 = (ImageView) findViewById(R.id.close6);
+
+
+
+        final Button ajout = (Button) findViewById(R.id.Ajouter);
 
 
         search.setOnClickListener(new View.OnClickListener() {
@@ -74,8 +97,169 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-                /*Intent intent = new Intent(MainActivity.this,MapsActivity.class);
-                startActivity(intent);*/
+                Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ajout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nbrAirport ++;
+                Log.w("ajout" , "on ajoute un aeroport" + nbrAirport);
+                if (nbrAirport == 2){
+                    airport2.setVisibility(View.VISIBLE);
+                    close2.setVisibility(View.VISIBLE);
+                }
+                if (nbrAirport == 3){
+                    airport3.setVisibility(View.VISIBLE);
+                    close3.setVisibility(View.VISIBLE);
+                }
+                if (nbrAirport == 4){
+                    airport4.setVisibility(View.VISIBLE);
+                    close4.setVisibility(View.VISIBLE);
+                }
+                if (nbrAirport == 5){
+                    airport5.setVisibility(View.VISIBLE);
+                    close5.setVisibility(View.VISIBLE);
+                }
+                if (nbrAirport == 6){
+                    airport6.setVisibility(View.VISIBLE);
+                    close6.setVisibility(View.VISIBLE);
+                    ajout.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+        close2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.w("supprimer" , "on supprime un aeroport" + nbrAirport);
+                if (nbrAirport == 2){
+                    nbrAirport --;
+                    airport2.setVisibility(View.GONE);
+                    close2.setVisibility(View.GONE);
+                }
+                if (nbrAirport == 3 ){
+                    nbrAirport --;
+                    airport3.setVisibility(View.GONE);
+                    close3.setVisibility(View.GONE);
+                    airport2.setText(airport3.getText());
+                }
+                if (nbrAirport == 4 ){
+                    nbrAirport --;
+                    airport4.setVisibility(View.GONE);
+                    close4.setVisibility(View.GONE);
+                    airport2.setText(airport3.getText());
+                    airport3.setText(airport4.getText());
+                }
+                if (nbrAirport == 5 ){
+                    nbrAirport --;
+                    airport5.setVisibility(View.GONE);
+                    close5.setVisibility(View.GONE);
+                    airport2.setText(airport3.getText());
+                    airport3.setText(airport4.getText());
+                    airport4.setText(airport5.getText());
+                }
+                if (nbrAirport == 6 ){
+                    nbrAirport --;
+                    airport6.setVisibility(View.GONE);
+                    close6.setVisibility(View.GONE);
+                    airport2.setText(airport3.getText());
+                    airport3.setText(airport4.getText());
+                    airport4.setText(airport5.getText());
+                    airport5.setText(airport6.getText());
+                    ajout.setVisibility(View.VISIBLE);
+                }
+            }
+
+        });
+
+        close3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.w("supprimer" , "on supprime un aeroport" + nbrAirport);
+                if (nbrAirport == 3){
+                    nbrAirport --;
+                    airport3.setVisibility(View.GONE);
+                    close3.setVisibility(View.GONE);
+                }
+                if (nbrAirport == 4 ){
+                    nbrAirport --;
+                    airport4.setVisibility(View.GONE);
+                    close4.setVisibility(View.GONE);
+                    airport3.setText(airport4.getText());
+                }
+                if (nbrAirport == 5 ){
+                    nbrAirport --;
+                    airport5.setVisibility(View.GONE);
+                    close5.setVisibility(View.GONE);
+                    airport3.setText(airport4.getText());
+                    airport4.setText(airport5.getText());
+                }
+                if (nbrAirport == 6 ){
+                    nbrAirport --;
+                    airport6.setVisibility(View.GONE);
+                    close6.setVisibility(View.GONE);
+                    airport3.setText(airport4.getText());
+                    airport4.setText(airport5.getText());
+                    airport5.setText(airport6.getText());
+                    ajout.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        close4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.w("supprimer" , "on supprime un aeroport" + nbrAirport);
+                if (nbrAirport == 4){
+                    nbrAirport --;
+                    airport4.setVisibility(View.GONE);
+                    close4.setVisibility(View.GONE);
+                }
+                if (nbrAirport == 5 ){
+                    nbrAirport --;
+                    airport5.setVisibility(View.GONE);
+                    close5.setVisibility(View.GONE);
+                    airport4.setText(airport5.getText());
+                }
+                if (nbrAirport == 6 ){
+                    nbrAirport --;
+                    airport6.setVisibility(View.GONE);
+                    close6.setVisibility(View.GONE);
+                    airport4.setText(airport5.getText());
+                    airport5.setText(airport6.getText());
+                    ajout.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        close5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.w("supprimer" , "on supprime un aeroport" + nbrAirport);
+                if (nbrAirport == 5){
+                    nbrAirport --;
+                    airport5.setVisibility(View.GONE);
+                    close5.setVisibility(View.GONE);
+                }
+                if (nbrAirport == 6 ){
+                    nbrAirport --;
+                    airport6.setVisibility(View.GONE);
+                    close6.setVisibility(View.GONE);
+                    airport5.setText(airport6.getText());
+                    ajout.setVisibility(View.VISIBLE);
+                }
+
+            }
+        });
+        close6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.w("supprimer" , "on supprime un aeroport" + nbrAirport);
+                airport5.setVisibility(View.GONE);
+                close5.setVisibility(View.GONE);
+                ajout.setVisibility(View.VISIBLE);
             }
         });
     }
