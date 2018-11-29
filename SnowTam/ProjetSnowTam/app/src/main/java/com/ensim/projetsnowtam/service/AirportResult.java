794b0de628c1dc1818331fc4e1a8eb6c31b091ea
Paul <paul.cordon@laposte.net>
2018-11-29 20:28:17 +0100

    plus qu'a implémenter les fonction de décodage une par une

--- SnowTam/ProjetSnowTam/app/src/main/java/com/ensim/projetsnowtam/service/AirportResult.java
+++ SnowTam/ProjetSnowTam/app/src/main/java/com/ensim/projetsnowtam/service/AirportResult.java
@@ -2,8 +2,10 @@ package com.ensim.projetsnowtam.service;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.util.Log;
 
-public class AirportResult implements Parcelable{
+public class AirportResult implements Parcelable {
+    private static final String TAG = "AirportResult";
     private String ICAO_Code;
     private double latitude;
     private double longitude;
@@ -14,15 +16,14 @@ public class AirportResult implements Parcelable{
     public AirportResult() {
     }
 
-    public AirportResult(String ICAO_Code, double latitude, double longitude, String snowtam,String snowtamDecoded,boolean isOnTarget) {
+    public AirportResult(String ICAO_Code, double latitude, double longitude, String snowtam, String snowtamDecoded, boolean isOnTarget) {
         this.ICAO_Code = ICAO_Code;
         this.latitude = latitude;
         this.longitude = longitude;
         this.snowtam = snowtam;
-        this.snowtamDecoded=snowtamDecoded;
-        this.isOnTarget=isOnTarget;
+        this.snowtamDecoded = snowtamDecoded;
+        this.isOnTarget = isOnTarget;
     }
-
 
 
     public String getICAO_Code() {
@@ -56,6 +57,7 @@ public class AirportResult implements Parcelable{
     public void setSnowtam(String snowtam) {
         this.snowtam = snowtam;
     }
+
     public boolean isOnTarget() {
         return isOnTarget;
     }
@@ -70,35 +72,140 @@ public class AirportResult implements Parcelable{
 
     public void setSnowtamDecoded(String snowtam) {
         //TODO décodé comme il faut
-        this.snowtamDecoded="";
-        for(String s : snowtam.split("\n")){
-            if(s.contains("A) ")){
-                this.snowtamDecoded = this.snowtamDecoded+"\nA) ";
-            }
-            else if(s.contains("B) ")){
-                this.snowtamDecoded = this.snowtamDecoded+"\nB) ";
-            }
-            else if(s.contains("C) ")){
-                this.snowtamDecoded = this.snowtamDecoded+"\nC) ";
-            }
-            else if(s.contains("D) ")){
-                this.snowtamDecoded = this.snowtamDecoded+"\nD) ";
-            }
-            else if(s.contains("E) ")){
-                this.snowtamDecoded = this.snowtamDecoded+"\nE) ";
-            }
-            else if(s.contains("F) ")){
-                this.snowtamDecoded = this.snowtamDecoded+"\nF) ";
-            }
-            else if(s.contains("G) ")){
-                this.snowtamDecoded = this.snowtamDecoded+"\nG) ";
-            }
-            else{
-                this.snowtamDecoded = this.snowtamDecoded+s;
-            }
+        this.snowtamDecoded = "";
+        for (String s : snowtam.split("\n")) {
+            this.snowtamDecoded = this.snowtamDecoded + decodeString(s) + "\n";
         }
     }
 
+
+    String decodeString(String s) {
+        String decoded = "";
+
+
+        if (s.contains("A) ")) {
+            decoded =  s + "\n";
+        }
+        if (s.contains("B) ")) {
+            decoded = "B) " + decodeB(s.split("B\\)")[1].split("(?=[A-Z])" + "\\)")[0])+"\n";
+        }
+        if (s.contains("C) ")) {
+            Log.d(TAG, "Le snowtam contien un C) ");
+            decoded = "C) " + decodeC(s.split("C\\)")[1].split("(?=[A-Z])" + "\\)")[0])+"\n";
+        }
+        if (s.contains("D) ")) {
+            decoded = "D) " + decodeD(s.split("D\\)")[1].split("(?=[A-Z])" + "\\)")[0])+"\n";
+        }
+        if (s.contains("E) ")) {
+            decoded = "E) " + decodeE(s.split("E\\)")[1].split("(?=[A-Z])" + "\\)")[0])+"\n";
+        }
+        if (s.contains("F) ")) {
+            decoded = "F) " + decodeF(s.split("F\\)")[1].split("(?=[A-Z])" + "\\)")[0])+"\n";
+        }
+        if (s.contains("G) ")) {
+            decoded = "G) " + decodeG(s.split("G\\)")[1].split("(?=[A-Z])" + "\\)")[0])+"\n";
+        }
+        if (s.contains("H) ")) {
+            decoded = "H) " + decodeH(s.split("H\\)")[1].split("(?=[A-Z])" + "\\)")[0])+"\n";
+        }
+        if (s.contains("J) ")) {
+            decoded = "J) " + decodeJ(s.split("J\\)")[1].split("(?=[A-Z])" + "\\)")[0])+"\n";
+        }
+        if (s.contains("K) ")) {
+            decoded = "K) " + decodeK(s.split("K\\)")[1].split("(?=[A-Z])" + "\\)")[0])+"\n";
+        }
+        if (s.contains("L) ")) {
+            decoded = "L) " + decodeL(s.split("L\\)")[1].split("(?=[A-Z])" + "\\)")[0])+"\n";
+        }
+        if (s.contains("M) ")) {
+            decoded = "M) " + decodeM(s.split("M\\)")[1].split("(?=[A-Z])" + "\\)")[0])+"\n";
+        }
+        if (s.contains("N) ")) {
+            decoded = "N) " + decodeN(s.split("N\\)")[1].split("(?=[A-Z])" + "\\)")[0])+"\n";
+        }
+        if (s.contains("P) ")) {
+            decoded = "P) " + decodeP(s.split("P\\)")[1].split("(?=[A-Z])" + "\\)")[0])+"\n";
+        }
+        if (s.contains("R) ")) {
+            decoded = "R) " + decodeR(s.split("R\\)")[1].split("(?=[A-Z])" + "\\)")[0])+"\n";
+        }
+        if (s.contains("S) ")) {
+            decoded = "S) " + decodeS(s.split("S\\)")[1].split("(?=[A-Z])" + "\\)")[0])+"\n";
+        }
+        if (s.contains("T) ")) {
+            decoded = "T) " + decodeT(s.split("T\\)")[1].split("(?=[A-Z])" + "\\)")[0])+"\n";
+        }
+        if(!(s.contains("A) ")||s.contains("B) ")||s.contains("C) ")||s.contains("D) ")||s.contains("E) ")||s.contains("F) ")||s.contains("G) ")||s.contains("H) ")||s.contains("J) ")||s.contains("K) ")||s.contains("L) ")||s.contains("M) ")||s.contains("N) ")||s.contains("P) ")||s.contains("R) ")||s.contains("S) ")||s.contains("T) "))){
+            decoded=s;
+        }
+
+        return decoded;
+    }
+
+    String decodeB(String s) {
+        return s;
+    }
+
+    String decodeC(String s) {
+        Log.d(TAG, s);
+        return "piste : " + s;
+    }
+
+    String decodeD(String s) {
+        return "DDDD" + s;
+    }
+
+    String decodeE(String s) {
+        return s;
+    }
+
+    String decodeF(String s) {
+        return s;
+    }
+
+    String decodeG(String s) {
+        return s;
+    }
+
+    String decodeH(String s) {
+        return "HHHH" + s;
+    }
+
+    String decodeJ(String s) {
+        return s;
+    }
+
+    String decodeK(String s) {
+        return s;
+    }
+
+    String decodeL(String s) {
+        return s;
+    }
+
+    String decodeM(String s) {
+        return s;
+    }
+
+    String decodeN(String s) {
+        return s;
+    }
+
+    String decodeP(String s) {
+        return s;
+    }
+
+    String decodeR(String s) {
+        return s;
+    }
+
+    String decodeS(String s) {
+        return s;
+    }
+
+    String decodeT(String s) {
+        return s;
+    }
 
 
     protected AirportResult(Parcel in) {
@@ -106,8 +213,8 @@ public class AirportResult implements Parcelable{
         latitude = in.readDouble();
         longitude = in.readDouble();
         snowtam = in.readString();
-        snowtamDecoded=in.readString();
-        isOnTarget= in.readByte() != 0;
+        snowtamDecoded = in.readString();
+        isOnTarget = in.readByte() != 0;
     }
 
     public static final Creator<AirportResult> CREATOR = new Creator<AirportResult>() {
@@ -121,6 +228,7 @@ public class AirportResult implements Parcelable{
             return new AirportResult[size];
         }
     };
+
     @Override
     public int describeContents() {
         return 0;
