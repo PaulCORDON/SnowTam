09e39fbd736f70480e8b75224b89ac28d33ddf12
Kilian sanchez <kilian0925@gmail.com>
2018-12-06 16:33:58 +0100

    commentage

--- SnowTam/ProjetSnowTam/app/src/main/java/com/ensim/projetsnowtam/service/Parametre.java
+++ SnowTam/ProjetSnowTam/app/src/main/java/com/ensim/projetsnowtam/service/Parametre.java
@@ -4,7 +4,7 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 import java.io.Serializable;
-
+/**  Classe des parametres de l'applcation*/
 public class Parametre implements Serializable, Parcelable {
 
 
